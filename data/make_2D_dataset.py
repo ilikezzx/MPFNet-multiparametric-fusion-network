cba36c79ece19ad4c5197d5b3a0b2b681b1cfd9a
zzx <1282878965@qq.com>
2022-08-06 23:15:00 +0800

    add files

--- data/make_2D_dataset.py
+++ data/make_2D_dataset.py
@@ -24,6 +24,59 @@ import numpy as np
         -- XXX
 """
 
+origin_dataset = r'C:\osteosarcoma\mergeData'
+new2D_dataset = r'C:\osteosarcoma\2D-dataset'
 
 if __name__ == '__main__':
-    pass
+    for patient_name in os.listdir(origin_dataset):
+        cnt = 1
+        path_one = os.path.join(origin_dataset, patient_name)
+        for series_name in os.listdir(path_one):
+            path_two = os.path.join(path_one, series_name)
+            t1_data_path = []
+            t2_data_path = []
+            for weight in os.listdir(path_two):
+                path_three = os.path.join(path_two, weight)
+                flag = 2
+                if 't1' in weight.lower():
+                    flag = 1
+                elif 't2' in weight.lower() or 'pd' in weight.lower():
+                    flag = 2
+                else:
+                    print('Wrong in', path_three)
+                    break
+
+                images_path = os.path.join(path_three, 'images')
+                masks_path = os.path.join(path_three, 'mask')
+                for image_name in os.listdir(images_path):
+                    if flag == 1:
+                        t1_data_path.append(
+                            (os.path.join(images_path, image_name), os.path.join(masks_path, image_name)))
+                    else:
+                        t2_data_path.append(
+                            (os.path.join(images_path, image_name), os.path.join(masks_path, image_name)))
+
+            if len(t1_data_path) != len(t2_data_path):
+                print('Wrong! in', path_two)
+                continue
+            else:
+                print('*' * 10, path_two, 'is perfect dataset', '*' * 10, f'   {cnt}')
+
+            for t1s, t2s in zip(t1_data_path, t2_data_path):
+                t1_image_path, t1_mask_path = t1s
+                t2_image_path, t2_mask_path = t2s
+
+                store_path = os.path.join(new2D_dataset, patient_name, str(cnt).zfill(3))
+                os.makedirs(store_path, exist_ok=True)
+
+                t1_mask, t2_mask = cv2.imread(t1_mask_path, cv2.IMREAD_GRAYSCALE), cv2.imread(t2_mask_path,
+                                                                                              cv2.IMREAD_GRAYSCALE)
+                u1, u2 = np.unique(t1_mask), np.unique(t2_mask)
+                if len(u1) <= 1 or len(u2) <= 1:
+                    continue
+
+                shutil.copy(t1_image_path, os.path.join(store_path, series_name + "_t1.png"))
+                shutil.copy(t1_mask_path, os.path.join(store_path, series_name + "_t1_mask.png"))
+                shutil.copy(t2_image_path, os.path.join(store_path, series_name + "_t2.png"))
+                shutil.copy(t2_mask_path, os.path.join(store_path, series_name + "_t2_mask.png"))
+                cnt += 1
--- hhah.py
+++
@@ -1,7 +0,0 @@
-"""
-# File       : hhah.py
-# Time       ：2022/7/30 22:19
-# Author     ：zzx
-# version    ：python 3.10
-# Description：
-"""
