1f321a7708e2410fa52652d5f8a2d799e9d6c749
zzx <1282878965@qq.com>
2022-08-16 21:26:39 +0800

    updata in 2022/08/16

---
+++ data/dcm2nrrd.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：bone-segmentation-1 
+@Author  ：zzx
+@Date    ：2022/8/16 20:02
+description:
+    dcm 转 nrrd 格式，以适应 pyradiomics
+"""
+
+import os
+import cv2
+import shutil
+import pydicom
+import imageio  # 转换成图像
+import numpy as np
+import SimpleITK as sitk
+
+
+def convert(dcm_dir, patient_name, mri_time, series_num):
+    series_IDs = sitk.ImageSeriesReader.GetGDCMSeriesIDs(dcm_dir)
+    # print(series_IDs)
+    if len(series_IDs) == 0: return
+
+    # 通过ID获取该ID对应的序列所有切片的完整路径， series_IDs[0]代表的是第一个序列的ID
+    # 如果不添加series_IDs[0]这个参数，则默认获取第一个序列的所有切片路径
+    series_file_names = sitk.ImageSeriesReader.GetGDCMSeriesFileNames(dcm_dir, series_IDs[0])
+    # print(series_file_names)
+
+    # 新建一个ImageSeriesReader对象
+    series_reader = sitk.ImageSeriesReader()
+
+    # 通过之前获取到的序列的切片路径来读取该序列
+    series_reader.SetFileNames(series_file_names)
+
+    # 获取该序列对应的3D图像
+    image3D = series_reader.Execute()
+
+    # 查看该3D图像的尺寸
+    print(image3D.GetSize())
+    store_path = os.path.join(dcm_dir, f'{patient_name}_{mri_time}_{series_num}.nrrd')
+    sitk.WriteImage(image3D, store_path)
+    print(f'finish convert in -- {store_path}')
+
+
+if __name__ == '__main__':
+    ori_dir = r'C:\Users\12828\Desktop\osteosarcoma\bone tumor data'
+    for patient_name in os.listdir(ori_dir):
+        second_path = os.path.join(ori_dir, patient_name)
+        for mri_time in os.listdir(second_path):
+            third_path = os.path.join(second_path, mri_time)
+            if os.path.isfile(third_path): continue
+            for series_num in os.listdir(third_path):
+                forth_path = os.path.join(third_path, series_num)
+                print(forth_path)
+                convert(forth_path, patient_name, mri_time, series_num)
--- practice/pyradiomic_test.py
+++ practice/pyradiomic_test.py
@@ -8,6 +8,69 @@ Description:
     有关 Pyradiomics的练习和测试
 """
 
+import nrrd
+import logging
+import SimpleITK as sitk
+import radiomics
+
+from radiomics import featureextractor
 
 if __name__ == '__main__':
-    pass
+    # Get some test data Download the test case to temporary files and return it's location. If already downloaded,
+    # it is not downloaded again, but it's location is still returned.
+    imageName = r'C:\Users\12828\Desktop\osteosarcoma\bone tumor data\1038501 qizhongyi\20140124MRI\S5 T2 tra STIR 1\1038501 qizhongyi_20140124MRI_S5 T2 tra STIR 1.nrrd'
+    maskName = r'C:\Users\12828\Desktop\osteosarcoma\bone tumor data\1038501 qizhongyi\20140124MRI\S5 T2 tra STIR 1\Untitled.nii.gz'
+
+    if imageName is None or maskName is None:  # Something went wrong, in this case PyRadiomics will also log an error
+        print('Error getting testcase!')
+        exit()
+
+    dicom = sitk.ReadImage(imageName)
+    # 像素比例长宽高信息
+    resampledPixelSpacing = dicom.GetSpacing()
+
+
+    # Regulate verbosity with radiomics.verbosity (default verbosity level = WARNING)
+    # radiomics.setVerbosity(logging.INFO)
+
+    # Get the PyRadiomics logger (default log-level = INFO)
+    logger = radiomics.logger
+    logger.setLevel(logging.DEBUG)  # set level to DEBUG to include debug log messages in log file
+
+    logger = logging.getLogger("radiomics.glcm")
+    logger.setLevel(logging.ERROR)
+
+    # Set up the handler to write out all log entries to a file
+    handler = logging.FileHandler(filename='testLog.txt', mode='w')
+    formatter = logging.Formatter("%(levelname)s:%(name)s: %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    # Define settings for signature calculation
+    # These are currently set equal to the respective default values
+    settings = {}
+    settings['binWidth'] = 25
+    settings['resampledPixelSpacing'] = resampledPixelSpacing
+    # [3,3,3] is an example for defining resampling (voxels with size 3x3x3mm)
+    settings['interpolator'] = sitk.sitkBSpline
+
+    # Initialize feature extractor
+    extractor = featureextractor.RadiomicsFeatureExtractor(**settings)
+
+    # By default, only original is enabled. Optionally enable some image types:
+    extractor.enableImageTypes(Original={}, LoG={}, Wavelet={})
+
+    # Disable all classes except firstorder
+    # extractor.disableAllFeatures()
+
+    # Enable all features in firstorder
+    # extractor.enableFeatureClassByName('firstorder')
+
+    # Only enable mean and skewness in firstorder
+    # extractor.enableFeaturesByName(firstorder=['Mean', 'Skewness'])
+
+    print("Calculating features")
+    featureVector = extractor.execute(imageName, maskName)
+
+    for featureName, featureValue in featureVector.items():
+        print("%s: %s" % (featureName, featureValue))
