17d35761daa7ea380531c6fbc5b26895b8e3d0d7
zzx <1282878965@qq.com>
2022-08-23 21:21:47 +0800

    update in 2022/08/23

--- predict_survival/feature_engineering.py
+++ predict_survival/feature_engineering.py
@@ -18,7 +18,9 @@ import logging
 import argparse
 import radiomics
 import numpy as np
+import pandas as pd
 import SimpleITK as sitk
+
 
 from radiomics import featureextractor
 
@@ -32,8 +34,11 @@ def get_feature(img_path, roi_path, is_T1=True):
     """
     # 处理 Mask
     # mask = sitk.ReadImage(roi_path)
-    # ori_mask_dirction = mask.GetDirection()
-    # ori_mask_Origin = mask.GetOrigin()
+    # ori_mask_direction = mask.GetDirection()
+    # ori_mask_origin = mask.GetOrigin()
+    # ori_mask_space = mask.GetSpacing()
+    #
+    # print(ori_mask_origin, ori_mask_direction)
     # mask = sitk.GetArrayFromImage(mask)
     #
     # u = np.unique(mask)
@@ -43,12 +48,16 @@ def get_feature(img_path, roi_path, is_T1=True):
     # else:
     #     mask[mask > 1] = 0      # 水肿区域不可看做肿瘤区域
     #
-    # mask = sitk.GetImageFromArray(mask)
-    # mask.SetDirection(ori_mask_dirction)
-    # mask.SetOrigin(ori_mask_Origin)
+    # out = sitk.GetImageFromArray(mask)
+    # out.SetDirection(ori_mask_direction)
+    # out.SetOrigin(ori_mask_origin)
+    # out.SetSpacing(ori_mask_space)
+    #
     # new_roi_path = roi_path[:-7]+"_new.nii.gz"
     # print(roi_path, new_roi_path)
-    # sitk.WriteImage(mask, new_roi_path, True)
+    # sitk.WriteImage(out, new_roi_path, True)
+    #
+    # return None
 
     dicom = sitk.ReadImage(img_path)
     # 像素比例长宽高信息
@@ -59,20 +68,10 @@ def get_feature(img_path, roi_path, is_T1=True):
     logger.setLevel(logging.DEBUG)  # set level to DEBUG to include debug log messages in log file
     logger = logging.getLogger("radiomics.glcm")
     logger.setLevel(logging.ERROR)
-
-    # Set up the handler to write out all log entries to a file
-    # handler = logging.FileHandler(filename='testLog.txt', mode='w')
-    # formatter = logging.Formatter("%(levelname)s:%(name)s: %(message)s")
-    # handler.setFormatter(formatter)
-    # logger.addHandler(handler)
-
     # Define settings for signature calculation
     # These are currently set equal to the respective default values
-    settings = {}
-    settings['binWidth'] = 25
-    settings['resampledPixelSpacing'] = resampledPixelSpacing
+    settings = {'binWidth': 25, 'resampledPixelSpacing': resampledPixelSpacing, 'interpolator': sitk.sitkBSpline}
     # [h,w,z] for defining resampling (voxels with size h x w x z mm)
-    settings['interpolator'] = sitk.sitkBSpline
     # Initialize feature extractor
     extractor = featureextractor.RadiomicsFeatureExtractor(**settings)
 
@@ -84,8 +83,9 @@ def get_feature(img_path, roi_path, is_T1=True):
     return featureVector
 
 
-def obtain_features(dataset_path, store_excel_path=r'./features.excel'):
+def obtain_features(dataset_path, store_excel_path=r'./features.csv'):
     t1 = time.time()
+    features_array = []
     for patient in os.listdir(dataset_path):
         path1 = os.path.join(dataset_path, patient)
         for mri_time in os.listdir(path1):
@@ -94,7 +94,7 @@ def obtain_features(dataset_path, store_excel_path=r'./features.excel'):
             for series in os.listdir(path2):
                 series_path = os.path.join(path2, series)
                 imgs = glob.glob(series_path+"/**.nrrd")
-                rois = glob.glob(series_path+"/**.nii.gz")
+                rois = glob.glob(series_path+"/**_new.nii.gz")
                 is_T1 = False
                 if 't1' in series.lower():
                     is_T1 = True
@@ -102,16 +102,23 @@ def obtain_features(dataset_path, store_excel_path=r'./features.excel'):
                 if len(imgs) == 0 or len(rois) == 0: continue
                 roi, img = rois[0], imgs[0]
                 print(roi, img)
-                get_feature(img, roi, is_T1=is_T1)
+                features = get_feature(img, roi, is_T1=is_T1)
+                features_array.append(features)
+                # print(features)
+
+    # store dict to store_excel_path
+    df = pd.DataFrame(features_array)
+    pd.DataFrame(df).to_csv(store_excel_path)
 
     t2 = time.time()
     print(f'Finish getting features, time:{t2-t1}')
 
 
+
 def get_args():
     parser = argparse.ArgumentParser(description='Hyperparameters settings')
-    parser.add_argument('--input-path', '-ip', type=str, default=r'C:\Users\12828\Desktop\osteosarcoma\bone tumor data', help='datasets path')
-    parser.add_argument('--store-path', '-sp', type=str, default=r'./features.excel', help='the path of output excel')
+    parser.add_argument('--input-path', '-ip', type=str, default=r'C:\osteosarcoma\bone tumor data', help='datasets path')
+    parser.add_argument('--store-path', '-sp', type=str, default=r'./features.csv', help='the path of output excel')
 
     return parser.parse_args()
 
